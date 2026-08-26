3ddb9680b1ac96615280fde6be77850a2f3a0c39
Jarad-Ayman-Rahman <jaradaymanr@gmail.com>
2026-08-26 15:09:06 +0300

    Olen tehnyt tehtäviä

---
+++ Moduuli 3/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+
+if pituus >= 140:
+  print("Pääset kaikkiin laitteisiin")
+
+  ika = int(input("Mikä on ikäsi? "))
+  if ika >= 8:
+    print("Pääset kaikkiin laitteisiin")
+  else:
+    print("Pääset kaikkiin paitsi Tulirekeen")
+
+elif pituus >= 100:
+  print("Pääset lasten laitteisiin")
+else:
+  print("Et pääse vielä mihinkään.")
---
+++ Moduuli 3/tuntitehtavat.py
@@ -0,0 +1,7 @@
+Vuosiluku_olympiavuosi = int(input("Anna vuosi:  "))
+
+if vuosi % 4 == 0 and vuosi != 2020:
+  print("Oli olympiavuosi")
+else:
+  print("Ei ollut olympiavuosi")
+  
